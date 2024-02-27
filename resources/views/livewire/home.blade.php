f88e7df2b2a710233e5fbca7dc9ddea6c76d0465
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-02-27 12:59:12 +0900

    ストーリーズアイコンを設定してレスポンシブ対応時横スクロールできるようにした。

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,8 +1,23 @@
-<div class="w-full">
+<div class="w-full h-full">
     {{--Header--}}
-    <header>
-
-
+    <header class="md:hidden sticky top-0 bg-white">
+        <div class="grid grid-cols-12 gap-2 items-center">
+            <div class="col-span-3">
+                <img src="{{asset('assets/logo.png')}}" alt="logo" class="h-12 w-full max-w-lg">
+            </div>
+            <div class="col-span-8 flex justify-center px-2">
+                <input type="text" placeholder="検索" class="border-0 outline-none w-full focus:outline-none bg-gray-100 rounded-lg focus:ring-0 hover:focus:ring-0">
+            </div>
+            <div class="col-span-1 flex justify-center">
+                <a href="#">
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.9" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                        </svg>
+                    </span>
+                </a>
+            </div>
+        </div>
     </header>
 
 
@@ -10,14 +25,54 @@
 
     {{--Main--}}
     <main class="grid lg:grid-cols-12 gap-8 md:mt-10">
-        <aside class="lg:col-span-8 border overflow-hidden h-[1000px]">
-
+        <aside class="lg:col-span-8 overflow-hidden">
+            {{--Stories--}}
+            <section>
+                <ul class="flex overflow-x-auto scrollbar-hide md:scrollbar-default items-center gap-2">
+                    @for($i=0; $i < 10; $i ++) <li class="flex flex-col justify-center w-20 gap-1 p-2">
+                        <x-avatar story src="http://source.unsplash.com/500x500?face-{{$i}}" class="h-14 w-14" />
+                        <p class="text-xs font-medium truncate">{{fake()->name}}</p>
+                        </li>
+                        @endfor
+                </ul>
+            </section>
         </aside>
 
-        {{--Suggestions--}}
 
-        <aside class="lg:col-span-4 border hidden lg:block p-4">
+        <aside class="lg:col-span-4  hidden lg:block p-4">
+            <div class="flex items-center gap-2">
+                <x-avatar src="http://source.unsplash.com/500x500?face" class="w-12 h-12" />
+                <h4 class="font-medium">{{fake()->name}}</h4>
+            </div>
 
+            {{--Suggestions--}}
+            <section class="mt-4">
+                <h4 class="font-bold text-gray-700/95">おススメユーザー</h4>
+                <ul class="my-2 space-y-3">
+                    @for($i=0; $i<5; $i++) <li class="flex items-center gap-3">
+                        <x-avatar src="http://source.unsplash.com/500x500?face-{{$i}}" class="h-12 w-12" />
+                        <div class="grid grid-cols-7 w-full gap-2">
+                            <div class="col-span-5">
+                                <h5 class="font-semibold truncate text-sm">{{fake()->name}}</h5>
+                                <p class="text-xm truncate">Followed by {{fake()->name}}</p>
+                            </div>
+                            <div class="col-span-2 flex text-right justify-end">
+                                <button class="font-bold text-blue-500 ml-auto text-sm">フォロー</button>
+                            </div>
+                        </div>
+                        </li>
+                        @endfor
+                </ul>
+            </section>
+
+            {{--App links--}}
+            <section class="mt-5">
+                <ol class="flex gap-2 flex-wrap">
+                    <li class="text-xs text-gray-800/90 font-medium"><a href="#" class="hover:underline">About</a>
+
+                    </li>
+                </ol>
+            </section>
         </aside>
     </main>
 
