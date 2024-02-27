c9f5bd9b824bd3675c9e3855e162f0d23ba0f801
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-02-27 16:45:03 +0900

    投稿画像が複数枚になった時にスワイプできるようにした

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,3 +10,31 @@ import intersect from '@alpinejs/intersect'
 
 Alpine.plugin(intersect)
 Alpine.start();
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+// import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+window.Swiper = Swiper;
+window.Navigation = Navigation;
+window.Pagination = Pagination;
+
+
+// Swiperの設定はbladeに直接記載するので以下は不要
+// // init Swiper:
+// const swiper = new Swiper('.swiper', {
+//     // configure Swiper to use modules
+//     modules: [Navigation, Pagination],
+//     // pagination設定
+//     pagination: {
+//         el: '.swiper-pagination',
+//     },
+//     // navigation設定
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+// });
