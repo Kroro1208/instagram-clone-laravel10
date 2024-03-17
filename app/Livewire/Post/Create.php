8378fe7c2b85c62c5f4176b57b9a4821fc840c98
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-03-17 21:42:33 +0900

    ファイルと画像が複数アップロードできるようにphp.ini及びlivewire.phpの設定を変更した

--- app/Livewire/Post/Create.php
+++ app/Livewire/Post/Create.php
@@ -2,10 +2,21 @@
 
 namespace App\Livewire\Post;
 
+use Livewire\WithFileUploads;
 use LivewireUI\Modal\ModalComponent;
 
 class Create extends ModalComponent
 {
+
+    use WithFileUploads;
+
+    public $media = [];
+    public $description;
+    public $location;
+    public $hide_like_view;
+    public $allow_commenting;
+
+
 
     /**
      * Supported: 'sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl', '6xl', '7xl'
@@ -13,6 +24,18 @@ class Create extends ModalComponent
     public static function modalMaxWidth(): string
     {
         return '4xl';
+    }
+
+    function submit()
+    {
+        dd([
+            $this->media,
+            $this->description,
+            $this->location,
+            $this->hide_like_view,
+            $this->allow_commenting,
+
+        ]);
     }
     public function render()
     {
