36a577a250aa56d8b655ece6b5c067e2866db3f9
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-03-19 17:02:21 +0900

    コメントを見る部分を押したらモーダルが表示されるようにした

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -11,6 +11,12 @@ class Home extends Component
 
     public $posts;
 
+    #[On('closeModal')]
+    function reverseUrl()
+    {
+        $this->js("history.replaceState({}, '', '/')");
+    }
+
     // 既にデータベースに保存されている投稿（Postオブジェクト）を特定のIDに基づいて検索し、
     // その投稿を現在の投稿リストの先頭に追加する
     #[On('post-created')]
